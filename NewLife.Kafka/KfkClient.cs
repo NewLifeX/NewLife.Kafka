bb92fea72ee30215a4c76e82efc970f582131769
大石头 <nnhy@vip.qq.com>
2020-06-04 21:57:46 +0800

    建立项目

---
+++ NewLife.Kafka/KfkClient.cs
@@ -0,0 +1,238 @@
+﻿using Confluent.Kafka;
+using NewLife.Collections;
+using NewLife.Log;
+using NewLife.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewLife.Kafka
+{
+    /// <summary>消费客户端</summary>
+    public class KfkClient : DisposeBase
+    {
+        #region 属性
+        /// <summary>是否使用中</summary>
+        public Boolean Active { get; private set; }
+
+        /// <summary>消费者</summary>
+        public IConsumer<String, String> Consumer { get; set; }
+
+        /// <summary>主题</summary>
+        public String Topic { get; set; }
+
+        /// <summary>消费者</summary>
+        public String GroupID { get; set; }
+
+        /// <summary>服务器集群地址</summary>
+        public String Servers { get; set; }
+
+        /// <summary>批大小。消费后整批处理，默认1000</summary>
+        public Int32 BatchSize { get; set; } = 1000;
+
+        /// <summary>每次消费完成后自动提交偏移量</summary>
+        public Boolean AutoCommited { get; set; } = true;
+
+        /// <summary>分区</summary>
+        protected IList<Int32> Partitions { get; } = new List<Int32>();
+        #endregion
+
+        #region 构造
+        /// <summary>销毁</summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            Stop();
+        }
+        #endregion
+
+        #region 开始停止
+        /// <summary>确保已创建</summary>
+        public virtual void EnsureCreate()
+        {
+            var csm = Consumer;
+            if (csm != null) return;
+
+            if (Topic.IsNullOrEmpty()) throw new Exception($"消费主题不能为空！");
+
+            var setting = LoadSetting();
+            //csm = new Consumer(setting);
+            var builder = new ConsumerBuilder<String, String>(setting.ToDictionary(e => e.Key, e => e.Value + ""));
+            csm = builder.Build();
+
+            //// 加载错误事件和消费错误事件处理函数
+            //csm.OnConsumeError += WriteLog;
+            //csm.OnError += WriteLog;
+            //csm.OnLog += WriteLog;
+
+            Consumer = csm;
+        }
+
+        /// <summary>开始</summary>
+        public void Start()
+        {
+            if (Active) return;
+
+            EnsureCreate();
+
+            // 挂载消费主题
+            var ps = Partitions;
+            if (ps != null && ps.Count > 0)
+            {
+                XTrace.WriteLine($"挂在分区总数：{ps.Count}，分区索引：{ps.Join(",")}");
+                Consumer.Assign(ps.Select(e => new TopicPartition(Topic, e)));
+            }
+            else
+            {
+                XTrace.WriteLine($"挂载全局,主题为：{Topic}");
+                Consumer.Subscribe(Topic);
+            }
+
+            InitStat();
+            _timer = new TimerX(DoConsume, null, 0, 1000) { Async = true };
+
+            Active = true;
+        }
+
+        /// <summary>停止</summary>
+        public void Stop()
+        {
+            if (!Active) return;
+
+            _timer.TryDispose();
+            _timer = null;
+
+            _stTimer.TryDispose();
+            _stTimer = null;
+
+            Consumer.Unassign();
+            Consumer.Unsubscribe();
+
+            XTrace.WriteLine("停止接收消息");
+
+            Active = false;
+        }
+        #endregion
+
+        #region 消费
+        private TimerX _timer;
+
+        private void DoConsume(Object state)
+        {
+            var list = new List<ConsumeResult<String, String>>();
+
+            // 多次拉取，批量处理
+            for (var i = 0; i < BatchSize; i++)
+            {
+                var result = Consumer.Consume(10);
+                if (result != null)
+                {
+                    list.Add(result);
+
+                    Stat.Increment(1, 0);
+                }
+            }
+
+            if (list.Count > 0)
+            {
+                // 批量处理
+                OnProcess(list.Select(e => e.Message).ToList());
+
+                // 提交确认
+                Consumer.Commit(list.Select(e => e.TopicPartitionOffset));
+
+                // 马上开始下一次
+                TimerX.Current.SetNext(-1);
+            }
+        }
+
+        /// <summary>收到消息事件</summary>
+        public event EventHandler<IList<Message<String, String>>> OnMessage;
+
+        /// <summary>处理一批消息</summary>
+        /// <param name="messages"></param>
+        protected virtual void OnProcess(IList<Message<String, String>> messages) => OnMessage?.Invoke(this, messages);
+        #endregion
+
+        #region 统计
+        /// <summary>消费统计</summary>
+        public ICounter Stat { get; set; } = new PerfCounter();
+
+        /// <summary>显示统计信息的周期。默认60秒，0表示不显示统计信息</summary>
+        public Int32 StatPeriod { get; set; } = 60;
+
+        private TimerX _stTimer;
+        private void InitStat()
+        {
+            var p = StatPeriod * 1000;
+            _stTimer = new TimerX(ShowStat, null, p, p) { Async = true };
+        }
+
+        private String _Last;
+        private void ShowStat(Object stat)
+        {
+            var sb = Pool.StringBuilder.Get();
+            var pf = Stat;
+            if (pf != null && pf.Value > 0) sb.AppendFormat("消费：{0} ", pf);
+
+            var msg = sb.Put(true);
+            if (msg.IsNullOrEmpty() || msg == _Last) return;
+            _Last = msg;
+
+            XTrace.WriteLine(msg);
+        }
+        #endregion
+
+        #region 辅助
+        /// <summary>
+        /// 加载设置
+        /// </summary>
+        /// <returns></returns>
+        protected virtual Dictionary<String, Object> LoadSetting()
+        {
+            var cfg = KfkSetting.Current;
+
+            //// 从配置中心读取集群地址
+            //if (Servers.IsNullOrEmpty()) Servers = ConfigClient.Instance.Get("Kafka.Server");
+
+            var dic = new Dictionary<String, Object>
+            {
+                { "group.id", GroupID },
+                { "bootstrap.servers", Servers },
+                { "enable.auto.commit", false },
+                { "auto.offset.reset",cfg.AutoReset}
+            };
+
+            // 设置属性
+            if (cfg.MaxMessages >= 0) dic.Add("consume.callback.max.messages", cfg.MaxMessages);
+            if (cfg.FetchMaxBytes >= 0) dic.Add("fetch.max.bytes", cfg.FetchMaxBytes);
+            if (cfg.MaxMessageBytes >= 0) dic.Add("message.max.bytes", cfg.MaxMessageBytes);
+            if (cfg.QMaxMessagesKbytes >= 0) dic.Add("queued.max.messages.kbytes", cfg.QMaxMessagesKbytes);
+            if (cfg.StoreSyncInterval >= 0) dic.Add("offset.store.sync.interval.ms", cfg.StoreSyncInterval);
+            //if (cfg.AutoCommitInterval >= 0) dic.Add("auto.commit.interval.ms", cfg.AutoCommitInterval);
+            if (cfg.FetchWaitTime >= 0) dic.Add("fetch.wait.max.ms", cfg.FetchWaitTime);
+            if (cfg.RecMessageMaxBytes >= 0) dic.Add("receive.message.max.bytes", cfg.RecMessageMaxBytes);
+            if (cfg.FetchMessageMaxBytes >= 0) dic.Add("fetch.message.max.bytes", cfg.FetchMessageMaxBytes);
+
+            return dic;
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void WriteLog(Object sender, Object e)
+        {
+            //if (e is Message<String, String> msg)
+            //    XTrace.Log.Error($"消费错误：{msg.Error} TopicPartitionOffset={msg.TopicPartitionOffset}");
+
+            if (e is Error msg2) XTrace.Log.Error(msg2.Reason);
+
+            if (e is LogMessage msg3) XTrace.WriteLine(msg3.Message);
+        }
+        #endregion
+    }
+}
---
+++ NewLife.Kafka/KfkSetting.cs
@@ -0,0 +1,47 @@
+﻿using NewLife.Xml;
+using System;
+using System.ComponentModel;
+
+namespace NewLife.Kafka
+{
+    /// <summary>设置</summary>
+    [XmlConfigFile("Config/Kafka.config", 15000)]
+    public class KfkSetting : XmlConfig<KfkSetting>
+    {
+        /// <summary>重新开始偏移量</summary>
+        [Description("重新开始偏移量 auto.offset.reset（smallest, earliest, beginning, largest, latest, end, error）")]
+        public String AutoReset { get; set; } = "earliest";
+
+        /// <summary>一次调度的最大消息数量"consume.callback.max.messages"</summary>
+        [Description("consume.callback.max.messages 一次调度的最大消息数量，（0 .. 1000000	）默认（0表示不限制）")]
+        public Int32 MaxMessages { get; set; }
+
+        /// <summary></summary>
+        [Description("最大消息字节 message.max.bytes")]
+        public Int32 MaxMessageBytes { get; set; } = 1_000_000;
+
+        /// <summary></summary>
+        [Description("receive.message.max.bytes")]
+        public Int32 RecMessageMaxBytes { get; set; } = 100_002_976;
+
+        /// <summary>1 .. 1000000000</summary>
+        [Description("fetch.max.bytes 0 .. 2147483135）")]
+        public Int32 FetchMaxBytes { get; set; } = 100_000_000;
+
+        /// <summary>1 .. 2097151</summary>
+        [Description("queued.max.messages.kbytes 队列最大消息大小（1 .. 2097151）")]
+        public Int32 QMaxMessagesKbytes { get; set; } = 1_048_576;
+
+        /// <summary>offset.store.sync.interval.ms 默认-1</summary>
+        [Description("偏移异步存储同步间隔 offset.store.sync.interval.ms")]
+        public Int32 StoreSyncInterval { get; set; } = -1;
+
+        /// <summary>抓取等待时间</summary>
+        [Description("fetch.wait.max.ms 抽取等待最大时间 0 .. 300000（默认100）")]
+        public Int32 FetchWaitTime { get; set; }
+
+        /// <summary>经过测试最大长度如果过长会导致抽取速度降低</summary>
+        [Description("fetch.message.max.bytes 抽取消息最大字节默认1mb，经过测试最大长度如果过长会导致抽取速度降低，建议降低最大消息长度")]
+        public Int32 FetchMessageMaxBytes { get; set; } = 524288;
+    }
+}
